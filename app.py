e9b1b4c7104f2ada387b6de7c2b52306a6874ebd
Nicholas  Chen <nicholaschen@Nicholass-Laptop.local>
2024-04-25 12:48:26 -0700

    template set up for form and summary

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# index will show an input for user to enter a url
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+# that will lead to a page that will summarize the url given by the user, with appropriate guardrail
+@app.route("/summary")
+def summary():
+    url = request.args.get('url')
+    return render_template('summary.html', url=url)
