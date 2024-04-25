975a546532236810f1fef0b35fc62811ec2df90e
Nicholas  Chen <nicholaschen@Nicholass-Laptop.local>
2024-04-25 15:24:38 -0700

    get text from html

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request
+import urllib.request
+import html2text
+
 app = Flask(__name__)
+h = html2text.HTML2Text()
+h.ignore_links = True
 
 # index will show an input for user to enter a url
 @app.route('/')
@@ -11,4 +16,14 @@ def index():
 @app.route("/summary")
 def summary():
     url = request.args.get('url')
+
+    fp = urllib.request.urlopen(url)
+    mybytes = fp.read()
+    html_content = mybytes.decode("utf8")
+    fp.close()
+    
+    content = h.handle(html_content)
+
+    print(content)
+
     return render_template('summary.html', url=url)
